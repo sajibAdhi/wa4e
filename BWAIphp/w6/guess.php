9e20d6c850ba633409907a9bd4a33fe152de9dc9
Shiftgithub <60180521+Shiftgithub@users.noreply.github.com>
2020-09-22 23:10:13 +0600

    Guess Game Update

--- BWAIphp/w6/guess.php
+++ BWAIphp/w6/guess.php
@@ -1,3 +1,19 @@
+<?php
+  $msg = null;
+  if ( ! isset($_GET['guess']) ) { 
+    $msg = "Missing guess parameter";
+  } else if ( strlen($_GET['guess']) < 1 ) {
+    $msg = "Your guess is too short";
+  } else if ( ! is_numeric($_GET['guess']) ) {
+    $msg = "Your guess is not a number";
+  } else if ( $_GET['guess'] < 26 ) {
+    $msg = "Your guess is too low";
+  } else if ( $_GET['guess'] > 26 ) {
+    $msg = "Your guess is too high";
+  } else {
+    $msg = "Congratulations - You are right";
+  }
+?>
 <html>
 <head>
 <title>Sajib Adhikary</title>
@@ -5,21 +21,7 @@
 <body>
 <h1>Welcome to my guessing game</h1>
 <p>
-<?php
-  if ( ! isset($_GET['guess']) ) { 
-    echo("Missing guess parameter");
-  } else if ( strlen($_GET['guess']) < 1 ) {
-    echo("Your guess is too short");
-  } else if ( ! is_numeric($_GET['guess']) ) {
-    echo("Your guess is not a number");
-  } else if ( $_GET['guess'] < 26 ) {
-    echo("Your guess is too low");
-  } else if ( $_GET['guess'] > 26 ) {
-    echo("Your guess is too high");
-  } else {
-    echo("Congratulations - You are right");
-  }
-?>
+<?= $msg ?>
 </p>
 </body>
 </html>

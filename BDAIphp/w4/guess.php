993f2b1ee528c5ea523bb05e42ddfb9ab044d356
Shiftgithub <60180521+Shiftgithub@users.noreply.github.com>
2020-09-23 01:46:21 +0600

    Guess Games

--- BDAIphp/w4/guess.php
+++ BDAIphp/w4/guess.php
@@ -6,48 +6,47 @@ $message = FALSE;
 
 if (isset($_POST['guess'])) {
 
-  $guess = $_POST['guess'] + 0;
-  $_SESSION['guess'] = $guess;
+    $guess = $_POST['guess'] + 0;
+    $_SESSION['guess'] = $guess;
 
-  $randomValue = 42;
-  if ($guess == $randomValue) {
-    $message = $_SESSION['message'] = "Great job, your guess is right.";
-  } elseif ($guess < $randomValue) {
-    $message = $_SESSION['message'] = "Your Guess is too Low";
-  } else {
-    $message = $_SESSION['message'] = "Your Guess is too High";
-  }
-  header("Location: guess2.php");
-  return;
+    $randomValue = 42;
+    if ($guess == $randomValue) {
+        $message = $_SESSION['message'] = "Great job, your guess is right.";
+    } elseif ($guess < $randomValue) {
+        $message = $_SESSION['message'] = "Your Guess is too Low";
+    } else {
+        $message = $_SESSION['message'] = "Your Guess is too High";
+    }
+    header("Location: guess2.php");
+    return;
 }
-echo "HEllo";
 ?>
 <html>
 
 <head>
-  <title>Sajib Adhikary</title>
+    <title>Sajib Adhikary</title>
 </head>
 
 <body>
-  <h1>Welcome to my guessing game</h1>
-  <p>Gussing Game.....</p>
-  <?php
-  $guess = isset($_SESSION['guess']) ? $_SESSION['guess'] : '';
-  $message = isset($_SESSION['message']) ? $_SESSION['message'] : false;
-  ?>
-  <p>
+    <h1>Welcome to my guessing game</h1>
+    <p>Gussing Game.....</p>
     <?php
-    if ($message !== FALSE) {
-      echo $message;
-    }
+    $guess = isset($_SESSION['guess']) ? $_SESSION['guess'] : '';
+    $message = isset($_SESSION['message']) ? $_SESSION['message'] : false;
     ?>
-  </p>
+    <p>
+        <?php
+        if ($message !== FALSE) {
+            echo $message;
+        }
+        ?>
+    </p>
 
-  <form method="POST">
-    <label for="guess"></label>
-    <input type="text" name="guess" id="guess" size="40" value="<?= $guess ?>">
-    <input type="submit">
-  </form>
+    <form method="POST">
+        <label for="guess"></label>
+        <input type="text" name="guess" id="guess" size="40" value="<?= $guess ?>">
+        <input type="submit">
+    </form>
 </body>
 
 </html>

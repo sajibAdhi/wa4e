205466320dd1e28d77269bb28a27f44f9d083d6b
Shiftgithub <60180521+Shiftgithub@users.noreply.github.com>
2020-09-27 00:48:05 +0600

    js

---
+++ JJJ/w1/js_03.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inline JS</title>
+</head>
+<body>
+    <h3>First Line on h3 Tag</h3>
+    <p><a href="js_01.php" onclick="alert('Why you clicked Here?'); return TRUE">Click Here</a></p>
+</body>
+</html>

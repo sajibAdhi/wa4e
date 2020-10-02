b519f3fa15ecb808e94245649e22bbc7a95385ef
Shiftgithub <60180521+Shiftgithub@users.noreply.github.com>
2020-10-02 15:07:00 +0600

    jjj w3 Assingment -> index Ui

---
+++ JJJ/w3_assingment/index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Sajib Adhikary - READ</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <?php require_once "link.php"; ?>
+</head>
+
+<body>
+    <div class="container">
+        <br>
+        <br>
+        <h1>Sajib Adhikary's Resume Registry</h1>
+        <br>
+        <br>
+        <span><?= empty($msg) ? '' : $msg ?></span>
+
+        <p><a href="login.php">Please Log In</a></p>
+    </div>
+    <?php require_once "script.php"; ?>
+</body>
+
+</html>

8cf8934992a1d4ec71162df4b309e581336f0546
Shiftgithub <60180521+Shiftgithub@users.noreply.github.com>
2020-10-02 20:20:50 +0600

    jjj w3 assingment -> add Ui

---
+++ JJJ/w3_assingment/add.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sajib Adhikary - CREATE</title>
+
+    <?php require_once "link.php"; ?>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Adding Profile for <?= empty($_SESSION['user']) ? '' : $_SESSION['user'] ?></h1>
+        <br>
+        <br>
+        <form>
+            <!-- First Name -->
+            <div class="form-group row">
+                <label for="first_name" class="col-md-3 col-form-label">First Name:</label>
+                <div class="col-md-9">
+                    <input type="text" class="form-control" name="first_name" id="first_name">
+                </div>
+            </div>
+            <!-- Last Name -->
+            <div class="form-group row">
+                <label for="last_name" class="col-md-3 col-form-label">Last Name:</label>
+                <div class="col-md-9">
+                    <input type="text" class="form-control" name="last_name" id="last_name">
+                </div>
+            </div>
+            <!-- email -->
+            <div class="form-group row">
+                <label for="email" class="col-md-3 col-form-label">Email:</label>
+                <div class="col-md-9">
+                    <input type="text" class="form-control" name="email" id="email">
+                </div>
+            </div>
+            <!-- HEADLINE -->
+            <div class="form-group row">
+                <label for="hline" class="col-md-3 col-form-label">Headline</label>
+                <div class="col-md-9">
+                    <input type="text" class="form-control" name="headline" id="headline">
+                </div>
+            </div>
+            <!-- Summary -->
+            <div class="form-group row">
+                <label for="summary" class="col-md-3 col-form-label">Summary</label>
+                <div class="col-md-9">
+                    <textarea class="form-control" name="summary" id="summary" rows="3"></textarea>
+                </div>
+            </div>
+            <!-- New  Positions -->
+            <div class="newPositions">
+                <div id="position">
+                    <div class="form-group row">
+                        <label class="col-md-3 col-form-label">Year</label>
+                        <div class="col-md-6">
+                            <input type="text" class="form-control" name="year">
+                        </div>
+                        <button type="button" class="btn btn-warning">-</button>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-3 col-form-label">Position</label>
+                        <div class="col-md-9">
+                            <input type="text" class="form-control" name="position">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- position -->
+            <div class="form-group row">
+                <label for="addPos" class="col-md-3 col-form-label">Position</label>
+                <div class="col-md-9">
+                    <button type="button" id="addPos" class="btn btn-warning">+</button>
+                </div>
+            </div>
+            <!-- Add Button -->
+            <div class="form-group row">
+                <div class="col-md-12">
+                    <button type="submit" class="btn btn-primary">Action</button>
+                </div>
+            </div>
+        </form>
+    </div>
+    <br>
+    <br>
+    <?php require_once "script.php"; ?>
+
+    <!-- Custom Script -->
+    <script>
+        countPos = 0;
+
+        $(document).ready(function() {
+            window.console && console.log('Document ready called');
+            $('#addPos').click(function(event) {
+
+                event.preventDefault();
+                if (countPos >= 9) {
+                    alert("Maximum of nine position entries exceeded");
+                    return;
+                }
+                countPos++;
+                window.console && console.log("Adding position " + countPos);
+                $('#position_fields').append(
+                    '<div id="position' + countPos + '"> \
+                        <p>Year: <input type="text" name="year' + countPos + '" value="" /> \
+                        <input type="button" value="-" \
+                        onclick="$(\'#position' + countPos + '\').remove();return false;"></p> \
+                        <textarea name="desc' + countPos + '" rows="8" cols="80"></textarea>\
+                    </div>'
+                );
+            });
+        });
+    </script>
+</body>
+
+</html>
